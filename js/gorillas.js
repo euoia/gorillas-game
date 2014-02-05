b2838585c23cbde62bc993b5452a95f4a6c474db
James Pickard <james.pickard+github@gmail.com>
2014-02-05 14:14:28 -0500

    canvas buildings with lots of hardcoded values

--- js/gorillas.js
+++ js/gorillas.js
@@ -4,12 +4,19 @@
 function Gorillas(options) {
   // Size of a grid square in pixels.
   this.gridSize = 20;
+  this.borderSize = 2;
 
   this.mapWidth = options.mapWidth;
   this.mapHeight = options.mapHeight;
 
-  // The jQuery selector for the screen element.
-  this.screen = '#'.concat(options.screen);
+  // TODO: Dynamically add the canvas element.
+  this.screen = document.getElementById(options.screen);
+  this.screen.style.width = this.toPixels(this.mapWidth) + 'px';
+  this.screen.style.height = this.toPixels(this.mapHeight) + 'px';
+  this.canvas = document.getElementById("c");
+  this.context = this.canvas.getContext('2d');
+  this.canvas.setAttribute('width', this.toPixels(this.mapWidth) + 'px');
+  this.canvas.setAttribute('height', this.toPixels(this.mapHeight) + 'px');
 
   // Easier to see when livereload has kicked off.
   console.log(Date.now());
@@ -19,9 +26,9 @@ function Gorillas(options) {
 }
 
 Gorillas.prototype.initScreen = function() {
-  $(this.screen).addClass('screen');
-  $(this.screen).css('width', this.toPixels(this.mapWidth));
-  $(this.screen).css('height', this.toPixels(this.mapHeight));
+  // x = 10, y = 20, width = 200, height = 100
+  this.context.fillStyle = 'blue';
+  this.context.fillRect(0, 0, this.toPixels(this.mapWidth), this.toPixels(this.mapHeight));
 };
 
 Gorillas.prototype.placeBuildings = function() {
@@ -58,16 +65,81 @@ Gorillas.prototype.placeBuilding = function(
   width,
   height
 ) {
-  var b = $('<div class="building" />').appendTo(this.screen);
-  b.css('left', this.toPixels(xpos));
-  b.css('top', this.toPixels(this.mapHeight - height));
-  b.css('height', this.toPixels(height));
-  b.css('width', this.toPixels(width) - 1);
-  b.css('position', 'absolute');
+  var colour = this.randomBuildingColour();
+  this.context.fillStyle = colour;
+  this.context.fillRect(this.toPixels(xpos),
+                        this.toPixels(this.mapHeight - height),
+                        this.toPixels(width) - this.borderSize,
+                        this.toPixels(this.mapHeight));
 
-  console.log("Placing building xpos=%d width=%d height=%d", xpos, width, height);
+  this.drawWindows(this.toPixels(xpos),
+                   this.toPixels(this.mapHeight - height),
+                   this.toPixels(xpos + width) - this.borderSize,
+                   this.toPixels(this.mapHeight));
+
+  console.log("Placing building colour=%s xpos=%d width=%d height=%d", colour, xpos, width, height);
+};
+
+Gorillas.prototype.drawWindows = function(xpos, ypos, xlim, ylim) {
+  var x = xpos + 8,
+    y = ypos + 8;
+
+  console.log("drawWindows xpos=%d ypos=%d xlim=%d ylim=%d", xpos, ypos, xlim, ylim);
+
+  while (x < xlim) {
+    y = ypos + 8;
+    while (y < ylim) {
+      if (x + 7 < xlim - 2 &&
+          y + 12 < ylim - 2
+      ) {
+        this.drawWindow(x, y);
+      }
+
+      y += 28;
+    }
+
+    x += 18;
+  }
+};
+
+Gorillas.prototype.drawWindow = function(xpos, ypos) {
+  var colour = this.randomWindowColour();
+  this.context.fillStyle = colour;
+  this.context.fillRect(xpos,
+                        ypos,
+                        7,
+                        12);
+
+  console.log("Drawing window at colour=%s xpos=%d ypos=%d", colour, xpos, ypos);
 };
 
 Gorillas.prototype.randomIntBetween = function(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
+
+Gorillas.prototype.randomBuildingColour = function() {
+  switch (this.randomIntBetween(0, 2)) {
+    case 0:
+      // Red.
+      return '#a10500';
+    case 1:
+      // Teal.
+      return '#14a0a3';
+    case 2:
+      // Grey.
+      return '#a2a0a2';
+  }
+};
+
+Gorillas.prototype.randomWindowColour = function() {
+  switch (this.randomIntBetween(0, 1)) {
+    case 0:
+      // Yellow.
+      return '#f8f503';
+    case 1:
+      // Grey.
+      return '#3d403d';
+  }
+};
+
+
